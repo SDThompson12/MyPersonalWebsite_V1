dfdb20e9627e5385809b105e2127441c556837d9
root <sean.d.thompson12@gmail.com>
2020-06-16 04:38:11 +0000

    Removed unnecessarily 3D Model file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 // '__dirname' - Just references to the current directory path Ex) .../workspace/seanjr-personaltests/MyPersonalSite
